5920e6f1b31c9a192e84cfccdd273d611047e7c0
samuelBright09 <samuelbrightsk@gmail.com>
2025-05-29 13:54:23 +0000

    Add configurations to bundle files, add theme toggle, setup husky for pre-commit hooking and render tools in UI

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,45 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/scripts/main.ts',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    }
+  entry: "./src/scripts/main.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+ module: {
+  rules: [
+    {
+      test: /\.ts$/,
+      use: 'ts-loader',
+      exclude: /node_modules/,
+    },
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.scss$/i,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    },
+  ],
+},
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+     new HtmlWebpackPlugin({ template: 'index.html' }),
+  new CopyWebpackPlugin({
+    patterns: [
+      { from: 'assets', to: 'assets' }, // copies ./assets into ./dist/assets
+    ],
+  }),
+  ],
+  mode: "development",
 };
